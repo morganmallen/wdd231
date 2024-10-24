353b03b71a587210d2f4c3c0a53bef6b78050432
Morgan Allen <morganallen1999@gmail.com>
2024-10-23 20:42:39 -0600

    Home page complete

---
+++ project/scripts/home.js
@@ -0,0 +1,33 @@
+//hamburger menu
+
+const hamburgerElement = document.querySelector("#menu-button");
+const navElement = document.querySelector("#animateMe");
+
+hamburgerElement.addEventListener("click", () => {
+  navElement.classList.toggle("open");
+  hamburgerElement.classList.toggle("open");
+});
+
+
+//footer
+
+const currentYear = new Date().getFullYear();
+document.getElementById(
+  "currentYear"
+).textContent = `© ${currentYear} Morgan's Soccer League`;
+
+const lastModification = new Date(document.lastModified);
+
+const month = String(lastModification.getMonth() + 1).padStart(2, "0");
+const day = String(lastModification.getDate()).padStart(2, "0");
+const year = lastModification.getFullYear();
+
+const hours = String(lastModification.getHours()).padStart(2, "0");
+const minutes = String(lastModification.getMinutes()).padStart(2, "0");
+const seconds = String(lastModification.getSeconds()).padStart(2, "0");
+
+const formattedDate = `${month}/${day}/${year} ${hours}:${minutes}:${seconds}`;
+
+document.getElementById(
+  "lastModified"
+).textContent = `Last Modification: ${formattedDate}`;
